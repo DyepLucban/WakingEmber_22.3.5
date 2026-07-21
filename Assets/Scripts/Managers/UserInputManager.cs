325b153a90ff755bbfde447b84132421d62f09c0
Jeffrey Lucban <lucbanjep@gmail.com>
2026-07-22 01:01:02 +0800

    Commented out old input manager

--- Assets/Scripts/Managers/UserInputManager.cs
+++ Assets/Scripts/Managers/UserInputManager.cs
@@ -1,57 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.InputSystem;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.InputSystem;
 
-public class UserInputManager : MonoBehaviour
-{
-    public static UserInputManager instance;
-    public UserInput userInput;
+// public class UserInputManager : MonoBehaviour
+// {
+//     public static UserInputManager instance;
+//     public UserInput userInput;
 
-    private InputAction playerMovement;
-    public InputAction playerJump { get; private set; }
-    public InputAction playerAttack { get; private set; }
+//     private InputAction playerMovement;
+//     public InputAction playerJump { get; private set; }
+//     public InputAction playerAttack { get; private set; }
 
-    private void Awake()
-    {
-        if (instance != null)
-            Destroy(gameObject);
-        else
-            instance = this;
+//     private void Awake()
+//     {
+//         if (instance != null)
+//             Destroy(gameObject);
+//         else
+//             instance = this;
 
-        userInput = new UserInput();
-    }
+//         userInput = new UserInput();
+//     }
 
-    private void OnEnable()
-    {
-        // Player Movements
-        playerMovement = userInput.Player.Movement;
-        playerMovement.Enable();
+//     private void OnEnable()
+//     {
+//         // Player Movements
+//         playerMovement = userInput.Player.Movement;
+//         playerMovement.Enable();
 
-        // Player Jump
-        playerJump = userInput.Player.Jump;
-        playerJump.Enable();
+//         // Player Jump
+//         playerJump = userInput.Player.Jump;
+//         playerJump.Enable();
 
-        // Player Attack
-        playerAttack = userInput.Player.Attack;
-        playerAttack.Enable();
-    }
+//         // Player Attack
+//         playerAttack = userInput.Player.Attack;
+//         playerAttack.Enable();
+//     }
 
-    private void OnDisable()
-    {
-        // Player Movements
-        playerMovement.Disable();
+//     private void OnDisable()
+//     {
+//         // Player Movements
+//         playerMovement.Disable();
 
-        // Player Jump
-        playerJump.Disable();
+//         // Player Jump
+//         playerJump.Disable();
 
-        // Player Attack
-        playerAttack.Disable();
-    }
+//         // Player Attack
+//         playerAttack.Disable();
+//     }
 
-    public void Update()
-    {
-        horizontalMovement();
-    }
-    public Vector2 horizontalMovement() => playerMovement.ReadValue<Vector2>();
-}
+//     public void Update()
+//     {
+//         horizontalMovement();
+//     }
+//     public Vector2 horizontalMovement() => playerMovement.ReadValue<Vector2>();
+// }
